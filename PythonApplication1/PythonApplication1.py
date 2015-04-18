1996c8c8990a1dd394d9e8d6b0dac524da68f2b6
Jerome Haltom <wasabi@cogito.cx>
2015-04-18 18:58:32 -0500

    Implement print methods properly. Add base class with print method. Add base class for PNExpr grammars which sums up their operation list in expressions.

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,114 +3,116 @@ from modgrammar import *
 
 grammar_whitespace_mode = 'optional'
 
-class Name (Grammar):
+class BaseGrammar (Grammar):
+
+    def print(self):
+        return ''
+
+class Identifier (BaseGrammar):
     grammar = (WORD('A-Za-z', 'A-Za-z0-9_'))
 
-    def print(self):
-        return self[0].string;
+    def grammar_elem_init(self, sessiondata):
+        self.id = self[0].string
 
-class Number (Grammar):
+    def print(self):
+        return self.id
+
+class Number (BaseGrammar):
     grammar = (WORD('0-9'))
 
-    def print(self):
-        return self[0].string;
-
-class ParenExpr (Grammar):
-    grammar = (L('('), REF('Expr'), L(')'))
+    def grammar_elem_init(self, sessiondata):
+        self.value = float(self[0].string)
 
     def print(self):
-        return '(' + self[1].print() + ')'
+        return str(self.value)
 
-class FuncExpr (Grammar):
-    grammar = (Name, L('['), REF('Expr'), L(']'))
+class Variable (BaseGrammar):
+    grammar = (Identifier,)
 
     def grammar_elem_init(self, sessiondata):
-        self.name = self[0].string
-        self.expr = self[2]
+        self.name = self[0].id
 
     def print(self):
-        return self.name + '[' + self.expr.print() + ']'
+        return self.name
 
-class ValueExpr (Grammar):
-    grammar = (OPTIONAL('-'), ParenExpr | FuncExpr | Name | Number)
+class Paren (BaseGrammar):
+    grammar = (L('('), REF('Expr'), L(')'))
 
-    def print(self):
-        negative = self[0] and self[0].string == '-'
-        if (negative):
-            return '-' + self[1].print();
-        else:
-            return self[1].print()
-
-class P0Term (Grammar):
-    grammar = (ValueExpr,)
+    def grammar_elem_init(self, sessiondata):
+        self.body = self[1]
 
     def print(self):
-        return self[0].print()
+        return '(' + self.body.print() + ')'
 
-class P0Expr (Grammar):
+class Func (BaseGrammar):
+    grammar = (Identifier, L('['), REF('Expr'), L(']'))
+
+    def grammar_elem_init(self, sessiondata):
+        self.name = self[0].id
+        self.body = self[2]
+
+    def print(self):
+        return self.name + '[' + self.body.print() + ']'
+
+class Value (BaseGrammar):
+    grammar = (OPTIONAL('-'), Paren | Func | Variable | Number)
+
+    def grammar_elem_init(self, sessiondata):
+        self.negative = self[0] and self[0].string == '-'
+        self.body = self[1]
+
+    def print(self):
+        return self.body.print()
+
+class Operation (BaseGrammar):
+
+    def grammar_elem_init(self, sessiondata):
+        self.expressions = [self[0]]
+        for e in self[1]:
+            self.expressions.append(e[0])
+            self.expressions.append(e[1])
+
+    def print(self):
+        value = ''
+        for e in self.expressions:
+            if e.print:
+                value += e.print()
+            else:
+                value += e.string
+        return value;
+
+
+class P0Term (BaseGrammar):
+    grammar = (Value,)
+    grammar_collapse = True
+
+class P0Expr (Operation):
     grammar = (P0Term, ONE_OR_MORE(L('^'), P0Term))
 
-    def print(self):
-        value = self[0].print()
-        for e in self[1]:
-            value += '^'
-            value += e[1].print()
-        return value;
+class P1Term (BaseGrammar):
+    grammar = (P0Expr | Value)
+    grammar_collapse = True
 
-
-class P1Term (Grammar):
-    grammar = (P0Expr | ValueExpr)
-
-    def print(self):
-        return self[0].print()
-
-class P1Expr (Grammar):
+class P1Expr (Operation):
     grammar = (P1Term, ONE_OR_MORE(L('/'), P1Term))
 
-    def print(self):
-        value = self[0].print()
-        for e in self[1]:
-            value += '/'
-            value += e[1].print()
-        return value;
+class P2Term (BaseGrammar):
+    grammar = (P0Expr | P1Expr | Value)
+    grammar_collapse = True
 
-class P2Term (Grammar):
-    grammar = (P0Expr | P1Expr | ValueExpr)
-
-    def print(self):
-        return self[0].print()
-
-class P2Expr (Grammar):
+class P2Expr (Operation):
     grammar = (P2Term, ONE_OR_MORE(L('*'), P2Term))
 
-    def print(self):
-        value = self[0].print()
-        for e in self[1]:
-            value += '*'
-            value += e[1].print()
-        return value;
+class P3Term (BaseGrammar):
+    grammar = (P0Expr | P1Expr | P2Expr | Value)
+    grammar_collapse = True
 
-class P3Term (Grammar):
-    grammar = (P0Expr | P1Expr | P2Expr | ValueExpr)
-
-    def print(self):
-        return self[0].print()
-
-class P3Expr (Grammar):
+class P3Expr (Operation):
     grammar = (P3Term, ONE_OR_MORE(L('+') | L('-'), P3Term))
 
-    def print(self):
-        value = self[0].print()
-        for e in self[1]:
-            if e[0].string == '+':
-                value += '+'
-            if e[0].string == '-':
-                value += '-'
-            value += e[1].print()
-        return value;
-
-class Expr (Grammar):
-    grammar = (P3Expr | P2Expr | P1Expr | P0Expr | ValueExpr)
+class Expr (BaseGrammar):
+    grammar = (P3Expr | P2Expr | P1Expr | P0Expr | Value)
+    grammar_collapse = True
 
     def print(self):
         return self[0].print()
@@ -121,4 +123,8 @@ if __name__ == '__main__':
         file = t.read()
         result = parser.parse_text(file, eof = True)
         print(result.print())
+
+        for e in result.find_all(Func):
+            if e.name == 'DiracDelta':
+                print(e.print())
     
